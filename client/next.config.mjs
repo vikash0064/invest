0343d53975e03d616888e6ff6cb34419ec73282b
vikash0064 <vikash@example.com>
2026-06-29 22:21:02 +0530

    Configure production env variables and start scripts

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000'}/api/:path*`,
       },
     ];
   },
